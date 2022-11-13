8597abccd5e8910bdffa753127d04c488cad8fb0
hanu607 <hanu607@gmail.com>
2022-11-13 21:03:40 +0900

    Make Todo-List

---
+++ js/clock.js
@@ -0,0 +1,16 @@
+const clock = document.querySelector("span#clock");
+const toDoDate = document.getElementById("todo-list__date");
+const days = ["SUNDAY","MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY"];
+const months = ["JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"];
+function getClock() {
+    const today = new Date();
+    const day = days[today.getDay()];
+    const month = months[today.getMonth()];
+    const date = String(today.getDate()).padStart(2,"0");
+    const hours = String(today.getHours()).padStart(2,"0");
+    const minutes =String(today.getMinutes()).padStart(2,"0");
+    clock.innerText = `${hours}:${minutes}`;
+    toDoDate.innerText = `${day}, ${month} ${date}`;
+}
+getClock(); //getClock을 처음에 바로 호출하기.
+setInterval(getClock, 1000)
---
+++ js/todo.js
@@ -0,0 +1,57 @@
+
+const toDoForm = document.getElementById("todo-list__form");
+const toDoInput = toDoForm.querySelector("input")
+const toDoList = document.getElementById("todo-list");
+const toDoClear = document.getElementById("todo-list__clear");
+const TODOS_KEY = "todos";
+let toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
+}
+function clearToDo(event) {
+  toDoList.querySelectorAll("li").forEach(li => li.remove());
+  toDos = [];
+  saveToDos();
+}
+function paintToDo(newToDoObj) {
+  const li = document.createElement("li");
+  li.id = newToDoObj.id;
+  const span = document.createElement("span");
+  span.innerText = newToDoObj.text;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
+}
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newToDo = toDoInput.value;
+  toDoInput.value = "";
+  const newToDoObj = {
+    text : newToDo,
+    id : Date.now()
+  };
+  toDos.push(newToDoObj);
+  paintToDo(newToDoObj);
+  saveToDos();
+}
+
+toDoForm.addEventListener("submit",handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  toDos.forEach(paintToDo);
+}
+
+toDoClear.addEventListener("click",clearToDo)
