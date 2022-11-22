692d25145b8afabbd2afb4acd82fb0087ec642c8
hanu607 <hanu607@gmail.com>
2022-11-22 17:28:10 +0900

    add YouTube Link & Google

--- js/apps.js
+++ js/apps.js
@@ -1,13 +1,15 @@
 const appLinks = document.querySelectorAll(".app-link");
+const youTube = document.querySelector(".youtube");
+const body = document.querySelector("body");
 
 function handleLinkClick(event) {
   event.preventDefault();
   const a = event.target.parentElement;
   const url = a.href;
   a.style.opacity = 0.5;
-  console.log(url);
   setTimeout(function() {
-    window.location.href = url;
+    window.open(url);
+    a.style.opacity = 1;
   }, 500);
 }
 appLinks.forEach(item=>{item.addEventListener("click", handleLinkClick)})
