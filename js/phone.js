9077d6c6998debe19f9c7e4d384b3a097c06772d
hanu607 <hanu607@gmail.com>
2022-11-25 14:43:25 +0900

    phone + phonecall

--- js/phone.js
+++ js/phone.js
@@ -1,10 +1,21 @@
 const phoneButton = document.querySelectorAll(".phone-button");
 const phoneNumber = document.querySelector(".phone__number");
+const callNumber = document.querySelector(".call__number");
 const eraseButton = document.querySelector(".phone-button__erase");
 const callButton = document.querySelector(".phone-button__call");
 const phoneScreen = document.querySelector(".phone-screen");
 const callScreen = document.querySelector(".call-screen");
+const phoneInputSpan = document.querySelector(".phone__input span");
+const callExit = document.querySelector(".call-button__exit");
 let nums = [];
+function fadeOn(el) {
+  el.style.display ="block";
+  el.style.animation = "fadeIn 0.5s linear 1";
+} 
+function fadeOff(el) {
+  el.style.animation = "fadeOut 0.5s linear 1";
+  setTimeout(function(){el.style.display ="none"},500);
+}
 function printNumber() {
   phoneNumber.innerText = '';
   if (nums.length > 11){
@@ -25,6 +36,7 @@ function printNumber() {
 }
 function call(){
   phoneScreen.style.animation ='fadeOut 0.3s linear 1';
+  callNumber.innerText = phoneNumber.innerText;
   callScreen.style.display ='block';
   setTimeout(function() {
     phoneScreen.style.display ='none';
@@ -32,11 +44,10 @@ function call(){
 }
 function button_click(event) {
   const button = event.target;
-  button.style.animation  ='none';
+  button.style.animation  = 'none';
   button.offsetHeight;
   button.style.animation = "clicked 0.5s linear 1";
   if (button == eraseButton) {
-    localStorage;
     nums.pop();
   }
   else if (button == callButton) {
@@ -44,5 +55,17 @@ function button_click(event) {
   }
   else nums.push(button.innerText);
   printNumber();
+  if (nums.length > 0) {
+    fadeOn(eraseButton);
+    fadeOn(phoneInputSpan);
+  }
+  else {
+    fadeOff(eraseButton);
+    fadeOff(phoneInputSpan);
+  }
 }
 phoneButton.forEach(item => item.addEventListener('click',button_click));
+
+callExit.addEventListener("click",function(event) {
+  window.close();
+});
